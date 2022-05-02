7441cb8230b60b5b8f1db0216ca746beac06bfa3
AdriaTorija <adriatorija@gmail.com>
2022-05-02 13:03:55 +0200

    :Outputs

--- SSD/ssd/modeling/ssd.py
+++ SSD/ssd/modeling/ssd.py
@@ -25,7 +25,8 @@ class SSD300(nn.Module):
         # Initialize output heads that are applied to each feature map from the backbone.
         
         for n_boxes, out_ch in zip(anchors.num_boxes_per_fmap, self.feature_extractor.out_channels):
-                
+            #Code 2.3.3
+              
             r1=nn.Sequential(
                 nn.BatchNorm2d(out_ch),
                 nn.LeakyReLU(0.2),
@@ -43,9 +44,10 @@ class SSD300(nn.Module):
                 nn.Conv2d(512, n_boxes * self.num_classes, kernel_size=3, padding=1),#sol
             )
            
-                
+            
+
             #r1=nn.Conv2d(out_ch, n_boxes * 4, kernel_size=3, padding=1)
-            #r2=(nn.Conv2d(out_ch, n_boxes * self.num_classes, kernel_size=3, padding=1)
+            #r2=nn.Conv2d(out_ch, n_boxes * self.num_classes, kernel_size=3, padding=1)
             self.regression_heads.append(r1)
             self.classification_heads.append(r2)
             
@@ -63,15 +65,17 @@ class SSD300(nn.Module):
             for param in layer.parameters():
                 if param.dim() > 1: nn.init.xavier_uniform_(param)
         
+        #Code 2.3.4
+        '''
+        
         if hasattr(self.classification_heads[-1],'bias'):
             last_layer = self.classification_heads[-1]
         elif hasattr(self.classification_heads[-1][-1],'bias'):
             last_layer = self.classification_heads[-1][-1]
-        
         nn.init.constant_(last_layer.bias.data, 0)
         background_class_fill = torch.log(torch.tensor(0.99 * (self.num_classes - 1)/(1 - 0.99)))
         nn.init.constant_(last_layer.bias.data[:len(last_layer.bias.data/self.num_classes)], background_class_fill)
-
+         '''
     def regress_boxes(self, features):
         locations = []
         confidences = []
